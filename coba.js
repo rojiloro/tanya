eb57ee22823f9783e6b2165edfdfd78126f5ef35
rojiloro <kelengoji79@gmail.com>
2023-04-03 08:26:16 +0700

    program jadi

--- coba.js
+++ coba.js
@@ -1,6 +1,15 @@
-function sholat() {
-  if (sholat == true) {
-    alert("hati menjadi tenang");
+var tanya = true;
+while (tanya) {
+  // inisiasi awal
+  var sholat = prompt("apakah sholatmu khusyuk? (iya/tidak) ");
+  // kondisi
+  if (sholat == "iya") {
+    alert("Hati menjadi tenang");
+  } else if (sholat == "tidak") {
+    alert("Hati Menjadi gelisah");
+  } else {
+    alert("Masukan iya atau tidak!");
   }
+
+  tanya = confirm("tanya dirimu lagi?");
 }
-sholat();
